43cf751b0e78e5de0e128bd2371971a00a548222
alexdum <alexandru.dumitrescu@gmail.com>
2021-04-28 09:30:50 +0300

    rename prec

--- sections/maps/maps.R
+++ sections/maps/maps.R
@@ -1,6 +1,11 @@
 
 rs <- reactive({
-  path <- paste0("www/data/ncs/changes_ensemble/bc_",input$Parameter,"_",input$Scenario,"_",input$Period,"_seasmean.nc")
+  
+  if (input$Parameter != "prAdjust") {
+    path <- paste0("www/data/ncs/changes_ensemble/bc_",input$Parameter,"_",input$Scenario,"_",input$Period,"_seasmean.nc")
+  } else {
+    path <- paste0("www/data/ncs/changes_ensemble/bc_",input$Parameter,"_",input$Scenario,"_",input$Period,"_seassum.nc")
+  }
   
   print(path)
   r <- brick(path)
@@ -18,62 +23,67 @@ rs <- reactive({
 })
 
 
-observe({
+
+
+
+
+output$coolplot <- renderPlot({
   
-  output$coolplot <- renderPlot({
+  
+  
+  rs <- rs()
+  print(rs)
+  
+  
+  
+  rg <- range(rs$values) %>% round(1)
+  brks <- seq(rg[1], rg[2], by = 0.2)
+  print(brks)
+  rs$values[rs$values > rg[2]] <- rg[2]
+  rs$values[rs$values < rg[1]] <- rg[1]
+  
+  
+  cols <- if (input$Parameter != "prAdjust") brewer.pal(9,"YlOrRd")
+  
+  ggplot() +
+    
+    geom_raster(data = rs, aes(x = x, y = y,
+                               fill = values),interpolate = T) +
+    geom_sf(fill = "transparent", data = judete) +
+    geom_sf_text(aes(label = JUDET),colour = "darkgrey",size = 3.15,data = judete)+
+    # make title bold and add space
+    # 
+    scale_fill_stepsn( colours = cols,
+                       name = "    °C", 
+                       breaks = brks) + 
     
     
     
-    rs <- rs()
-    print(rs)
+    # scale_fill_gradientn(name = "    °C",
+    #                      limits = c(rg[1], rg[2]),
+    #                      breaks = brks,
+    #                      colors = cols)+
     
-    rg <- range(rs$values) %>% round(1)
-    brks <<- seq(rg[1], rg[2], by = 0.2)
-    print(brks)
-    rs$values[rs$values > rg[2]] <- rg[2]
-    rs$values[rs$values < rg[1]] <- rg[1]
-    
-    
-    # cols <- c("#8c6bb1","#9ecae1","#deebf7","#ffffe5",brewer.pal(9,"YlOrRd"),rev(brewer.pal(9,"PuRd")))
-    
-    cols <<- "YlOrRd"
-    
-    
-    print(cols)
-    
-    ggplot() +
-      
-      geom_raster(data = rs, aes(x = x, y = y,
-                                 fill = values),interpolate = T) +
-      geom_sf(fill = "transparent", data = judete) +
-      geom_sf_text(aes(label = JUDET),colour = "darkgrey",size = 3.15,data = judete)+
-      # make title bold and add space
-      # 
-      # scale_fill_gradientn(name = "    °C",
-      #                      limits = c(rg[1], rg[2]),
-      #                      breaks = brks,
-      #                      colors = cols)+ 
-      
-      colorspace::scale_fill_binned_sequential(palette = as.character(cols), name = "    °C", breaks = brks) + 
-      # change color scale
-      #geom_tile(data = hill_spdf, aes(x = x, y = y, fill = value)) +
-      #scale_fill_gradient(low = "black", high = "white") +
-      #new_scale_fill() +
-      #geom_tile(data = dem_spdf, aes(x = x, y = y, fill = value), alpha=0.4)
-      
-      labs(caption = "Database: @MeteoRomania") +
-      xlab("") + ylab("") +
-      theme_bw() +
-      guides(fill = guide_colourbar(barwidth = 1.0, barheight = 14.0, title.position = "top")) +
-      theme( legend.position = "right" )   #c(.93,.72)
-    
-    # output$downloadPlot <- downloadHandler(
-    #   filename = function(){paste("input$coolplot",'.png',sep='')},
-    #   content = function(file){
-    #     ggsave(file,plot=input$coolplot)
-    #   }
-    # )
-    
-  })
+    #colorspace::scale_fill_binned_sequential(palette =  values$c, name = "    °C", breaks = brks) + 
+    # change color scale
+    #geom_tile(data = hill_spdf, aes(x = x, y = y, fill = value)) +
+  #scale_fill_gradient(low = "black", high = "white") +
+  #new_scale_fill() +
+  #geom_tile(data = dem_spdf, aes(x = x, y = y, fill = value), alpha=0.4)
+  
+  labs(caption = "Database: @MeteoRomania") +
+    xlab("") + ylab("") +
+    theme_bw() +
+    guides(fill = guide_colourbar(barwidth = 1.0, barheight = 14.0, title.position = "top")) +
+    theme( legend.position = "right" )   #c(.93,.72)
+  
+  # output$downloadPlot <- downloadHandler(
+  #   filename = function(){paste("input$coolplot",'.png',sep='')},
+  #   content = function(file){
+  #     ggsave(file,plot=input$coolplot)
+  #   }
+  # )
   
 })
+
+
