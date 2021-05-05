e13e96c2df8a1650d9282ab6d1006121bdda8d69
alexdum <alexandru.dumitrescu@gmail.com>
2021-05-05 13:43:08 +0300

    tabs url

--- server.R
+++ server.R
@@ -1,5 +1,6 @@
 server <- shinyServer(function(input, output, session) {
   
+  source(file = "utils/tabs_url.R", local = T)
    #observeEvent(input$RegiuneInput,
     #            updateSelectInput(session, "judetInput", "Judet",
      #                             choices = unique(date$NUME[date$CMR==input$RegiuneInput])))
