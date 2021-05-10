5e9da1f30710bc159bd0d2871ef4ff19f0de39c4
alexdum <alexandru.dumitrescu@gmail.com>
2021-05-10 23:52:57 +0300

    improve maps

--- sections/maps/maps.R
+++ sections/maps/maps.R
@@ -58,9 +58,13 @@ plotInput<- reactive ({
   
   rs <- rs()$change %>% rasterToPoints() %>% as_tibble()
   names(rs)[3] <- "values"
-  
   rg <- range(rs$values) %>% round(1)
   
+  rm <- rs()$enmean %>% rasterToPoints() %>% as_tibble()
+  names(rm)[3] <- "values"
+  rg.mean <- range(rm$values) %>% round(1)
+  
+
   # simboluri in functie de parametru
   if (input$Parameter != "prAdjust") {
     ylOrBn <- colorRampPalette( brewer.pal(9,"YlOrRd"), interpolate="linear")
@@ -69,9 +73,9 @@ plotInput<- reactive ({
     lim <- c(0.5, 5.5)
     # pentru hartile cu schimbarea
     rmean <- colorRampPalette( brewer.pal(11, "RdYlBu")[3:11], interpolate="linear")
-    brks.change <- seq(-8, 26, by = 2)
-    cols.change <- rmean(length(brks.change) - 1)
-    lim.change <- c(-6, 24)
+    brks.mean <- seq(-8, 26, by = 2)
+    cols.mean <- rmean(length(brks.mean) - 1)
+    lim.mean <- c(-6, 24)
     
   } else {
     cols <- brewer.pal(6,"BrBG")
@@ -80,19 +84,63 @@ plotInput<- reactive ({
     # pentru hartile cu schimbarea
     rmean <- colorRampPalette( c(brewer.pal(9, "YlOrBr")[3:9], brewer.pal(9, "BuPu")), interpolate="linear")
     if (input$Season != "Annual") {
-    brks.change <- c(0,5,10,20,30,40,50,75,100)
-    cols.change <- rmean(length(brks.change) - 1)
-    lim.change <- c(5, 75)
+    brks.mean <- c(0,5,10,20,30,40,50,75,100)
+    cols.mean <- rmean(length(brks.mean) - 1)
+    lim.mean <- c(5, 75)
     } else {
-      brks.change <- c(300,400,500,600,700,800,900,1000)
-      cols.change <- rmean(length(brks.change) - 1)
-      lim.change <- c(400, 900)
+      brks.mean <- c(300,400,500,600,700,800,900,1000)
+      cols.mean <- rmean(length(brks.mean) - 1)
+      lim.mean <- c(400, 900)
     }
     
   }
   
   #print(cols)
   #print(rg)
+  
+  plot.mean <- ggplot() +
+    geom_raster(data = rm, aes(x = x, y = y,
+                               fill = values),interpolate = F, alpha = 100) +
+    geom_sf(fill = "lightgrey", color = "grey", data = ctrs) +
+    geom_sf(fill = "transparent", data = judete) +
+    geom_sf(fill = "#a4b9b9", data = sea, color = "lightgrey", lwd = 0.4) +
+    geom_sf_text(aes(label = name),colour = "darkgrey",size = 3, data = judete) + 
+    geom_vline(xintercept = c(20,22,24,26,28,30), color="#EBEBEB", linetype='dashed') +
+    geom_hline(yintercept = c(44,45,46,47,48), color="#EBEBEB", linetype='dashed') +
+    
+    annotation_raster(logo, xmin = 20.525, xmax = 21.525, ymin = 43.9, ymax = 44.5) +
+    # make title bold and add space
+    # 
+    scale_fill_stepsn( colours = cols.mean,
+                       name = ifelse(input$Parameter != "prAdjust", "      °C", "      %"), 
+                       breaks = brks.mean,
+                       limits = lim.mean) + 
+    labs(caption = paste("@SUSCAP", Sys.Date()), title = textvar(), x = "", y = "") +
+    coord_sf(xlim = c(20,30), ylim = c(43.5, 48.5), expand = F) +
+    theme_bw() + #xlim(20,30) + ylim(43.7, 48.3) +
+    guides(fill =  guide_colourbar(barwidth = 1.0, barheight = 9, title.position = "top",
+                                   label.theme = element_text(size = 10))) +
+    scale_linetype_manual(values=c("twodash")) +
+    
+    theme(legend.position = c(.94, .91),
+          legend.justification = c("right", "top"),
+          legend.background = element_rect(fill="lightgrey", colour = "lightgrey"),
+          plot.caption = element_text(vjust = 30, hjust = 0.040, size = 7.5),
+          plot.title = element_text(vjust = -7.5, hjust = 0.5, size = 13),
+          #plot.title = element_blank(),
+          #plot.caption = element_blank(),
+          #axis.text = element_blank(),
+          axis.title = element_blank(),
+          axis.ticks = element_blank(),
+          #axis.ticks.length = unit(0, "pt"), #length of tick marks
+          panel.grid.major=element_blank(),
+          panel.grid.minor=element_blank(),
+          plot.margin = margin(0, 0, 0, 0, "cm")
+    ) +
+    annotate("text", label = paste("min.:", rg.mean[1] %>% sprintf("%.1f",.)), x=29.1, y = 46, size = 3.3) +
+    annotate("text", label = paste("avg.:", mean(rm$values) %>% round(1)%>% sprintf("%.1f",.)), x = 29.1, y = 45.9,  size = 3.3) +
+    annotate("text", label = paste("max.:", rg.mean[2] %>% sprintf("%.1f",.)), x=29.1, y = 45.8, size = 3.3)
+  
   
   plot.change <- ggplot() +
     geom_raster(data = rs, aes(x = x, y = y,
@@ -117,33 +165,41 @@ plotInput<- reactive ({
     guides(fill =  guide_colourbar(barwidth = 1.0, barheight = 9, title.position = "top",
                                    label.theme = element_text(size = 10))) +
     scale_linetype_manual(values=c("twodash")) +
-    
+    last_plot() + labs(x=NULL, y=NULL) +
     theme(legend.position = c(.94, .91),
           legend.justification = c("right", "top"),
           legend.background = element_rect(fill="lightgrey", colour = "lightgrey"),
           plot.caption = element_text(vjust = 30, hjust = 0.040, size = 7.5),
           plot.title = element_text(vjust = -7.5, hjust = 0.5, size = 13),
+          #plot.title = element_blank(),
+          #plot.caption = element_blank(),
           #axis.text = element_blank(),
           axis.title = element_blank(),
           axis.ticks = element_blank(),
-          axis.ticks.length = unit(0, "pt"), #length of tick marks
-          # panel.grid.major=element_blank(),
-          # panel.grid.minor=element_blank(),
-          plot.margin = margin(-0.9, 0.5, 0, 0, "cm")
+          #axis.ticks.length = unit(0, "pt"), #length of tick marks
+          panel.grid.major=element_blank(),
+          panel.grid.minor=element_blank(),
+          plot.margin = margin(0, 0, 0, 0, "cm")
     ) +
     annotate("text", label = paste("min.:", rg[1] %>% sprintf("%.1f",.)), x=29.1, y = 46, size = 3.3) +
     annotate("text", label = paste("avg.:", mean(rs$values) %>% round(1)%>% sprintf("%.1f",.)), x = 29.1, y = 45.9,  size = 3.3) +
     annotate("text", label = paste("max.:", rg[2] %>% sprintf("%.1f",.)), x=29.1, y = 45.8, size = 3.3)
   
- list(plot.change = plot.change)
+ list(plot.change = plot.change, plot.mean = plot.mean)
   
 })
 
 # pentru randare plot
-output$coolplot <- renderPlot(
-  width = 850, height = 650, units = "px",res = 100, {
+output$plot.change <- renderPlot(
+  width = 900, height = 670, units = "px",res = 110, {
     plotInput()$plot.change
   })
+
+output$plot.mean <- renderPlot(
+  width = 900, height = 670, units = "px",res = 110, {
+    plotInput()$plot.mean
+  })
+
 
 
 # render plot asimage
@@ -167,21 +223,37 @@ output$coolplot <- renderPlot(
 
 
 # pentru descarcare plot imagine
-output$downloadPlot <- downloadHandler(
+output$downpchange <- downloadHandler(
   filename = function() { paste(textvar() %>% gsub(" " ,"_", . ) %>% gsub("vs.", "vs",.) %>% tolower(), '.png', sep='') },
   content = function(file) {
-    png(file, width = 850, height = 650, units = "px", res = 100)
+    png(file, width = 900, height = 670, units = "px", res = 110)
     print(plotInput()$plot.change)
     dev.off()
   })
 
 # pentru descarcare fisier Geotiff
-output$downloadRaster <- downloadHandler(
+output$downrchange <- downloadHandler(
   filename = function() { paste(textvar() %>% gsub(" " ,"_", . ) %>% gsub("vs.", "vs",.) %>% tolower(), '.tif', sep='') },
   content = function(file) {
     writeRaster(rs()$change, file, overwrite = T)
-  }
-)
+  })
+  
+  
+  # pentru descarcare plot imagine
+  output$downpmean <- downloadHandler(
+    filename = function() { paste(textvar() %>% gsub(" " ,"_", . ) %>% gsub("vs.", "vs",.) %>% tolower(), '.png', sep='') },
+    content = function(file) {
+      png(file, width = 900, height = 670, units = "px", res = 110)
+      print(plotInput()$plot.mean)
+      dev.off()
+    })
+  
+  # pentru descarcare fisier Geotiff
+  output$downrmean <- downloadHandler(
+    filename = function() { paste(textvar() %>% gsub(" " ,"_", . ) %>% gsub("vs.", "vs",.) %>% tolower(), '.tif', sep='') },
+    content = function(file) {
+      writeRaster(rs()$mean, file, overwrite = T)
+    })
 
 
 
