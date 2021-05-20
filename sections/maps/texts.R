c62ab895f67be91f672709df4950bc404eedb611
alexdum <alexandru.dumitrescu@gmail.com>
2021-05-20 15:17:45 +0300

    rext tidy

--- sections/maps/texts.R
+++ sections/maps/texts.R
@@ -16,12 +16,12 @@ text.maps <- reactive({
   if (season == "Annual") season <- tolower(season)
   unit <- ifelse(param == "precipitation", "%", "°C")
   
- text.change <-  paste("Calculated change in" ,season, param, "(",unit,") for the period",period ," 
-  compared with 1971-2000. The map is based on an ensemble with ten climate scenarios 
+ text.change <-  paste0("Calculated change in" ,season," ",param, " (",unit,") for the period ",period ,"
+                        compared with 1971-2000. The map is based on an ensemble with ten climate scenarios 
   for the ",scen," scenario. The maps below show more information about the ensembles averages 
   over two period of times.")
   
- text.anom <- paste("The diagram shows the calculated change in" ,season, param, "(",unit,") in 
+ text.anom <- paste0("The diagram shows the calculated change in " ,season, " ",param, " (",unit,") in 
                     Romania during the years 1971-2100 compared with normal (mean for 1971-2100). 
                     The bars show historic data from observations. The black line shows the ensemble
                     mean of ten climate scenarios. The grey field shows the range in variation between the highest 
