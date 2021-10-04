e3e026c84b01c966eabc38f3ec3ffe85fb5bade3
alexdum <alexandru.dumitrescu@gmail.com>
2021-10-04 10:40:27 +0300

    tidy code

--- sections/maps/details_settings.R
+++ sections/maps/details_settings.R
@@ -16,9 +16,9 @@ if (reg_param != "prAdjust" & strsplit(reg_period,"_")[[1]][1] == "mean") {
   
   pal <- colorBin(cols, domain = shape$values, bins = bins)
   pal2 <- colorBin(cols, domain = shape$values, bins = bins, reverse = T)
-  print(bins)
-  print(cols)
-  print(range(shape$values))
+  # print(bins)
+  # print(cols)
+  # print(range(shape$values))
 }
 
 
