7505db2aab3f500882911217d950fe6685563eb8
alexdum <alexandru.dumitrescu@gmail.com>
2021-10-10 19:18:50 +0300

    change slider color

--- ui.R
+++ ui.R
@@ -30,8 +30,13 @@ ui <- shinyUI(
                   "body {padding-top: 70px;}",
                   # responsive images
                   "img {max-width: 100%; width: 100%; height: auto}",
+                  # sliders color toate 3
+                  ".js-irs-0 .irs-to,.js-irs-0 .irs-from , .js-irs-0 .irs-bar-edge, .js-irs-0 .irs-bar {background: #E95420; border-color: #E95420;}",
+                  ".js-irs-1 .irs-to,.js-irs-1 .irs-from , .js-irs-1 .irs-bar-edge, .js-irs-1 .irs-bar {background: #E95420; border-color: #E95420;}",
+                  ".js-irs-2 .irs-to,.js-irs-2 .irs-from , .js-irs-2 .irs-bar-edge, .js-irs-2 .irs-bar {background: #E95420; border-color: #E95420;}"
                   # inaltime navbaer
                   #'.navbar-brand{display:none;}'
+                  #'
                 )
                 
               ),
