6dc92e9bb28e59ece76a5c3179dab71d59917e1b
alexdum <alexandru.dumitrescu@gmail.com>
2021-11-14 21:01:35 +0200

    indicator description

--- utils/map_func.R
+++ utils/map_func.R
@@ -157,6 +157,25 @@ leg_leaf_ind <- function (input, reg_period, param) {
   pal2 <- colorBin(cols, domain = df.col$values, bins = bins, reverse = T)
   return(list(pal = pal, pal2 = pal2, leaflet_titleg = leaflet_titleg))
   
+}
+
+indicators_def <- function(indicators) {
+  switch (
+    which(c("heatuspring","heatufall","scorchno","scorchu", "coldu","frostu10", "frostu15","frostu20","prveget", "prfall", "prwinter" ) %in%  indicators),
+    
+    text.desc <- "Cumulative heat units (ΣTmed. > 0°C) in the period 01 February - 10 April",
+    text.desc <- "Cumulative heat units (ΣTmed. > 0°C) in the period 01 September - 31 October",
+    text.desc <- "Scorching heat units (ΣTmax. ≥ 32°C) from 1 June to 31 August",
+    text.desc <- "Scorching heat number of days (Tmax. ≥ 32°C) from 1 June to 31 August",
+    text.desc <- "Cold units (ΣTmed. < 0°C) cumulated during the period 01 November - 31 March",
+    text.desc <- "Frost units (ΣTmin. ≤ -10°C) cumulated in the period 01 December - 28/29 February",
+    text.desc <- "Frost units (ΣTmin. ≤ -15°C) cumulated in the period 01 December - 28/29 February",
+    text.desc <- "Frost units (ΣTmin. ≤ -20°C) cumulated in the period 01 December - 28/29 February",
+    text.desc <- "Precipitatin amounts (l/m²) during the autumn wheat growing season, 01 September to 30 June",
+    text.desc <- "Precipitation amounts (l/m²) during the autumn sowing period, 01 September - 31 October",
+    text.desc <- "Precipitation amounts (l/m²) during the soil water accumulation period, 01 November - 31 March",
+  )
+  return(text.desc)
   
 }
 
